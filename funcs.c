dcf224e93dbf27a51548e719c53ee85b2907a6cd
Subat Ahsan <subat.ahsan18@gmail.com>
2024-09-21 04:16:37 -0400

    Updated structure

--- funcs.c
+++
@@ -1,9 +0,0 @@
-void draw_player(Player p){
-	for (short x = p.x; x<playerSize; x++){
-        for (short y = p.y; y<playerSize; y++){
-            plot_pixel(x,y, p.color);
-        }
-    }
-	
-	
-};
